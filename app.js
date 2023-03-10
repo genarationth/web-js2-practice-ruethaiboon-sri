76a96f9855ecdbf195492c2ef2f3a7b47cb6de3a
P <ruethaiboon.sri@gmail.com>
2023-03-10 22:35:43 +0700

    Challenges 1-2

---
+++ app.js
@@ -0,0 +1,14 @@
+//Challenge 1: Create an Age Calculator
+let myAge = 23
+let futureYear = 2028
+let thisYear = 2023
+console.log(`I will be ${(myAge + (futureYear-thisYear))} years old`)
+
+//Challenge 2: Create an information card
+let employee = {
+    Name: "Aitana",
+    Location: "Madrid",
+    Hobby: "Singing"
+}
+let info = `Hi, my name is ${employee.Name}. I live in ${employee.Location} and enjoy ${employee.Hobby}`
+console.log(info);
